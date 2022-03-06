c4d118942735f43d82213e581b2904af618a92a2
IanoSang <ianosang18@gmail.com>
2022-03-06 12:16:43 +0300

    feat: script- adds a form validation script

--- js/script.js
+++ js/script.js
@@ -4,16 +4,23 @@ let date = document.getElementById('date');
 let year = document.getElementById('year');
 let generateAkan =  document.getElementById("generateAkan");
 
-generateAkan.addEventListener('click', function (e){
-    e.preventDefault()
-    console.log(month.value)
-})
-console.log(month.value)
-function AkanName (){
+
+function akanName (){
     let CC = year.value.slice(0,2);//Century Digit
     let YY = year.value.slice(2,4);// Year Digit
     let MM = month.value;
     let DD = date.value;
     let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-    return d;
+    return (d);
+    console.log(d);
+
+
+
 }
+generateAkan.addEventListener('click', function (e){
+    e.preventDefault()
+    if (month.value =="" && year.value =="" && date.value =="") {
+        alert("Please Enter you credentials");
+        return false;
+    }
+})
