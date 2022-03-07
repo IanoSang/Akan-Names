854846fb5cdf9d2310b2f17620ac2e81fa49cafb
IanoSang <ianosang18@gmail.com>
2022-03-07 14:19:11 +0300

    patch: js- corrects the algoriths used and adds the correct the funtionality

--- js/script.js
+++ js/script.js
@@ -1,42 +1,45 @@
-// Data Picker Initialization
-let month = document.getElementById('month');
-let date = document.getElementById('date');
-let year = document.getElementById('year');
-let generateAkan =  document.getElementById("generateAkan");
 
-
-
-generateAkan.addEventListener('click', function (e){
-    e.preventDefault()
-    if (month.value == "" && year.value =="" && date.value =="") {
-        alert("Please Enter you credentials");
-        return false;
-    }
-    else if (year.value <= 0) {
-        alert("invalid Year of Birth! ")
-    }
-
-    else if ((month.value < 1) || (month.value > 31)) {
-        alert(" invalid Month! ")
-    }
-
-    else if (date.value < 0 || date.value > 31) {
-
-        alert("invalid Date of Birth! ")
-    }
-
+function formRefresh(){
+    document.getElementById("form_data").reset();
 }
-)
-function akanName (){
-    let CC = year.value.slice(0,2);//Century Digit
-    let YY = year.value.slice(2,4);// Year Digit
-    let MM = month.value;
-    let DD = date.value;
-    let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-    return (d);
-    let index = d.toFixed();
-    alert(index);
+
+// Event function on button
+function akanNames() {
+
+    let birthDate = document.getElementById("birthdate").value;
+    let dateEntered = birthDate.split("-");
+    let day = parseInt(dateEntered[2]);
+    let month = parseInt(dateEntered[1]);
+    let year = parseInt(dateEntered[0]);
+
+    //Centuary calculation
+    let centry = (year - 1) / 100 + 1;
+
+    let dayOfTheWeek = (centry / 4 - 2 * centry - 1 + 5 * year / 4 + 26 * (month + 1) / 10 + day) % 7;
+
+    document.getElementById("display").innerHTML = Math.round(dayOfTheWeek);
+    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    let maleNames = ["Kwasi", " Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let gender = document.getElementById("gender").value;
+    let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dOfWeek = Math.round(dayOfTheWeek)
+
+    //form validation and output
+    if (!isNaN(dOfWeek)) {
+        if (gender == "Male") {
+            document.getElementById("display").innerHTML = "Your akan names is " + maleNames[Math.round(dayOfTheWeek)] + " and you were born on " + daysOfWeek[dOfWeek];
+        } else if (gender === "Female") {
+            document.getElementById("display").innerHTML = "You were born on " + daysOfWeek[dOfWeek] + " and Your akan name is " + femaleNames[Math.round(dayOfTheWeek)];
+        } else {
+            alert("Enter valid dates and choose your gender");
+            document.getElementById("display").innerHTML = "";
+        }
+    } else {
+        alert("Enter valid birthday and choose your gender");
+        document.getElementById("display").innerHTML = "";
+    }
 
 
-
+    //call to the reFresh function
+    formRefresh();
 }
